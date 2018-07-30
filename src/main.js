060f237e6db3a1265c68b8f6ce4c94b2de4c38ee
Vasily <gerasimovv1990@gmail.com>
2018-07-30 16:25:59 +0300

    continue

--- src/main.js
+++ src/main.js
@@ -1,34 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+let objects = [
+  {
+    likes: 1,
+    comments: 10,
+    img: "https://saray.ru/i/00066591-0.jpg"
+  },
+  {
+    likes: 17,
+    comments: 180,
+    img:
+      "http://bazakartin.ru/wp-content/uploads/2015/08/tulpani-rozovie-kvadr-min.jpg"
+  },
+  {
+    likes: 1,
+    comments: 10,
+    img: "https://i10.fotocdn.net/s8/219/gallery_m/323/2241675994.jpg"
+  },
+  {
+    likes: 16,
+    comments: 210,
+    img:
+      "http://wallpapersdig.com/images/preview/Snow-Mountain-Wallpaper-8473-Hd-Wallpapers-Nature-66944.jpg"
+  },
+  {
+    likes: 14,
+    comments: 310,
+    img: "https://cdn-img.jamendo.com/albums/s92/92946/covers/1.600.jpg"
+  },
+  {
+    likes: 167,
+    comments: 120,
+    img:
+      "https://s3.amazonaws.com/kmpz-p/images/i441120-7726765815279396409.jpg"
+  },
+  {
+    likes: 21,
+    comments: 12,
+    img:
+      "https://image1ws.indotrading.com/s3/productimages/co30890/s10268/w600-h600/a4c83126-a5cf-4465-b7b2-2a83bf6d97bfw.jpg"
+  },
+  {
+    likes: 42,
+    comments: 150,
+    img:
+      "https://cdn-img.jamendo.com/albums/s56/56931/covers/1.600.jpg?du=1500992307"
+  },
+  {
+    likes: 31,
+    comments: 150,
+    img: "http://nabeel911.com/wp-content/uploads/ildikoneerdreamland4.jpeg"
+  },
+  {
+    likes: 36,
+    comments: 150,
+    img:
+      "http://www.radioandmusic.com/sites/www.radioandmusic.com/files/images/biz/2015/11/27/Image-%2837%29.jpg"
+  }
+];
 
 class ContentHolder extends React.Component {
   render() {
-    return (
-      <div class="inside-block">
-          <div class="hidden">
-            <span class="group">
-            <span class="likes"><img src="https://cdn.pixabay.com/photo/2017/06/24/20/27/heart-2438744__180.png" alt=""></span>
-            <span class="">1</span>
+    objects.forEach(obj => {
+      return (
+        <div className="inside-block">
+          <div className="hidden">
+            <span className="group">
+              <span className="likes">
+                <img
+                  src="https://cdn.pixabay.com/photo/2017/06/24/20/27/heart-2438744__180.png"
+                  alt=""
+                />
+              </span>
+              <span className="">${obj.likes}</span>
             </span>
-            <span class="group">
-              <span class="comments"><img src="http://www.qartrock.com/wp-content/uploads/2017/04/111.png" alt=""></span>
-              <span class="">10</span>
+            <span className="group">
+              <span className="comments">
+                <img
+                  src="http://www.qartrock.com/wp-content/uploads/2017/04/111.png"
+                  alt=""
+                />
+              </span>
+              <span className="">{obj.comments}</span>
             </span>
           </div>
-          <img src="https://saray.ru/i/00066591-0.jpg" alt=""></div>
-    )
+          <img src={obj.url} alt="" />
+        </div>
+      );
+    });
+    return objects;
+    // return <h1>Hello World!</h1>;
   }
 }
-
-
 
 function render() {
   ReactDOM.render(
     <div class="main-div">
       <ContentHolder />
     </div>,
-    document.getElementById('root'),
+    document.getElementById("root")
   );
 }
 
